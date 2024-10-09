4e9da2d3ebbe470f6a77f9e7ae00757d886854fd
Aviv Vegh <avegh@hsf.net>
2024-10-08 21:23:08 -0700

    add fonts

---
+++ web/src/app/login/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+export default function LoginPage() {
+  const onSigninClick = () => {
+    location.href = "http://localhost:3002/auth/linkedin";
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-indigo-200/100	">
+      <div className="flex flex-col items-center">
+        <h1 className="font-title text-3xl font-mono font-medium text-center">
+          Welcome to AI Resume Builder
+        </h1>
+        <p className="font-title text-l text-pretty w-2/4 font-medium text-center mt-10 font-mono">
+          {`We’re excited to have you here! Our AI Resume Builder is designed to simplify and enhance your job application process. Whether you're a seasoned professional or just starting out, our tool will guide you in crafting a polished, professional resume that stands out from the crowd. 
+          Using the power of AI, we tailor each section of your resume to highlight your skills, experience, and achievements in a way that resonates with recruiters and hiring managers. Say goodbye to hours of formatting and writing—let our intelligent platform do the heavy lifting for you.
+          Get ready to showcase your talents and take the next step in your career journey with confidence!`}
+        </p>
+        <div className="flex flex-row justify-center items-center mt-5">
+          <div
+            className="flex flex-wrap border-2 p-1 bg-white hover:bg-sky-700 cursor-pointer"
+            onClick={onSigninClick}
+          >
+            <p className="font-medium font-mono text-l mr-0.5">Sign in with</p>
+            <img
+              className="w-6 h-6"
+              src="/images/linkedin.png"
+              alt="LinkedIn"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-export default function Home() {
-  const onSigninClick = () => {
-    location.href = "http://localhost:3002/auth/linkedin";
-  };
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-  return (
-    <div className="flex justify-center justify-items-center justify-self-center">
-      <div className="flex flex-col ">
-        <h1 className="text-align: center font-title mt-20 text-2xl font-medium	justify-self-center	">
-          Welcome to AI Resume Builder
-        </h1>
-        <div className="flex flex-row justify-center align-middle	items-center mt-5">
-          <div
-            className="flex flex-wrap border-2 p-1 bg-white  hover:bg-sky-700 "
-            onClick={onSigninClick}
-          >
-            <p className="font-medium text-l mr-0.5 ">Sign in with</p>
-            <img className="size-6 " src="/images/linkedin.png" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+export default function Main() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, []);
+
+  return <></>;
 }
