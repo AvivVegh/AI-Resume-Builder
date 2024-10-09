f999ad713ce6dcdc4aea2a7f5d391bfbd4f884da
Aviv Vegh <avegh@hsf.net>
2024-10-08 20:43:01 -0700

    add linkedin button

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        title: ["open-sans", "sans-serif"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
